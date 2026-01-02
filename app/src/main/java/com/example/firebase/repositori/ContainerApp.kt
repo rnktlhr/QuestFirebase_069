b5432be5f7f67817a30a8e5e6f015e105d7254b5
rnktlhr <rinakitluhur10@gmail.com>
2026-01-02 14:04:13 +0700

    membuat file dan code container app

---
+++ app/src/main/java/com/example/firebase/repositori/ContainerApp.kt
@@ -0,0 +1,22 @@
+package com.example.firebase.repositori
+
+import android.app.Application
+
+interface ContainerApp {
+    val repositorySiswa: RepositorySiswa
+}
+
+class DefaultContainerApp : ContainerApp {
+    override val repositorySiswa: RepositorySiswa by lazy {
+        FirebaseRepositorySiswa()
+    }
+}
+
+class AplikasiDataSiswa : Application() {
+    lateinit var container: ContainerApp
+
+    override fun onCreate() {
+        super.onCreate()
+        this.container = DefaultContainerApp()
+    }
+}
