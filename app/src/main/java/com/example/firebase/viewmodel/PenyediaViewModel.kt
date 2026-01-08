f46d70d2a0206f2eae4541bd9743d34641db73ef
rnktlhr <rinakitluhur10@gmail.com>
2026-01-08 12:01:40 +0700

    memperbaiki PenyediaViewModel.kt

--- app/src/main/java/com/example/firebase/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/firebase/viewmodel/PenyediaViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.firebase.viewmodel
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
@@ -20,6 +21,18 @@ object PenyediaViewModel {
         }
         initializer {
             EntryViewModel(
+                aplikasiDataSiswa().container.repositorySiswa
+            )
+        }
+        initializer {
+            DetailViewModel(
+                createSavedStateHandle(),
+                aplikasiDataSiswa().container.repositorySiswa
+            )
+        }
+        initializer {
+            EditViewModel(
+                createSavedStateHandle(),
                 aplikasiDataSiswa().container.repositorySiswa
             )
         }
