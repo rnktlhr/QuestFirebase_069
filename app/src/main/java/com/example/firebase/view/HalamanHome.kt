30d022100503b8671540a9ac3ad8c0ca4bc800cd
rnktlhr <rinakitluhur10@gmail.com>
2026-01-08 12:00:00 +0700

    menambah halaman home

--- app/src/main/java/com/example/firebase/view/HalamanHome.kt
+++ app/src/main/java/com/example/firebase/view/HalamanHome.kt
@@ -99,7 +99,7 @@ fun HomeBody(
             is StatusUiSiswa.Loading -> LoadingScreen()
             is StatusUiSiswa.Success -> DaftarSiswa(
                 itemsSiswa = statusUiSiswa.siswa,
-                onSiswaClick = { it.id.toInt() }
+                onSiswaClick = { onSiswaClick(it.id.toInt()) }
             )
             is StatusUiSiswa.Error -> ErrorScreen(
                 retryAction = retryAction,
@@ -144,7 +144,7 @@ fun DaftarSiswa(
     onSiswaClick: (Siswa) -> Unit,
     modifier: Modifier = Modifier
 ) {
-    LazyColumn(modifier = Modifier) {
+    LazyColumn(modifier = modifier) {
         items(items = itemsSiswa, key = { it.id }) { person ->
             ItemSiswa(
                 siswa = person,
